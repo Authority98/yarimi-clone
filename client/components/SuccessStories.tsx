0273d0fe6a8648c84e176d5e90973851dc15f62e
Authority98 <usamashabbier@gmail.com>
2025-11-02 19:39:46 +0500

    Fix SuccessStories video embedding to match main video implementation

--- client/components/SuccessStories.tsx
+++ client/components/SuccessStories.tsx
@@ -3,46 +3,52 @@ import React from 'react';
 const SuccessStories: React.FC = () => {
   const testimonials = [
     {
-      id: '0hz7dm834l',
-      embedCode: `<iframe src="https://fast.wistia.net/embed/iframe/0hz7dm834l?videoFoam=true" title="Student Success Story" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"></iframe>`,
-      testimonial: "Thanks to you, I'm now $200K funded. One month later... And I was $250K+ funded on top of that.",
-      name: "Asim",
-      title: "Funded Trader"
+      id: 1,
+      videoId: "nzcftmm46w",
+      stat: "30%",
+      testimonial: "increase in revenue and it's consistent monthly. We started to do numbers we've not seen before. Just one video brought us both revenue and reach.",
+      name: "Chef Gero DiMaria",
+      title: "Founder of Kucina"
     },
     {
-      id: 'zrex34rj3h',
-      embedCode: `<iframe src="https://fast.wistia.net/embed/iframe/zrex34rj3h?videoFoam=true" title="Student Success Story" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"></iframe>`,
-      testimonial: "In 4 months, I've managed to get the strategy right, get my psychology in place and make a withdrawal.",
-      name: "Sadia",
-      title: "Successful Trader"
+      id: 2,
+      videoId: "zz8c7lzku4",
+      stat: "600K",
+      testimonial: "views and brought in nearly 4,000 new followers. Working with BrandTok has been a game-changer. I'm very, very satisfied. This is the only platform we use that brings us so many customers.",
+      name: "Abdullah",
+      title: "Royal Fragrances"
     },
     {
-      id: 'dro6dt8f29',
-      embedCode: `<iframe src="https://fast.wistia.net/embed/iframe/dro6dt8f29?videoFoam=true" title="Student Success Story" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"></iframe>`,
-      testimonial: "My student Eranio got funded a $100K account and I flew him out.",
-      name: "Eranio",
-      title: "$100K Funded Trader"
+      id: 3,
+      videoId: "nusik7g4p8",
+      stat: "80%",
+      testimonial: "of our new customers came from TikTok. My advice to anyone watching – just go for it. We would have made so much more money had we worked with BrandTok earlier.",
+      name: "Nor & Amir",
+      title: "Owners of Salt n Bake"
     },
     {
-      id: 'fiflicslh8',
-      embedCode: `<iframe src="https://fast.wistia.net/embed/iframe/fiflicslh8?videoFoam=true" title="Student Success Story" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"></iframe>`,
-      testimonial: "Developed good habits. Provide for my family. I quit my job. Last event I became funded, now I've made $15,000 in withdrawals.",
-      name: "Yarimi University Event",
-      title: "Event Attendee"
+      id: 4,
+      videoId: "29709myyaw",
+      stat: "200K+",
+      testimonial: "views in 2 weeks. We had customers coming in even on the slower days. It's really good for us. The content has impacted our business positively.",
+      name: "Samsuddin",
+      title: "Owner of Kampong Chai Chee Restaurant"
     },
     {
-      id: 'akal1tbdsf',
-      embedCode: `<iframe src="https://fast.wistia.net/embed/iframe/akal1tbdsf?videoFoam=true" title="Student Success Story" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"></iframe>`,
-      testimonial: "The discord helped me a lot. Seeing other members doing their analysis. I entered a real account and made $18K profit.",
-      name: "Linda",
-      title: "Real Account Trader"
+      id: 5,
+      videoId: "ipe02t12l6",
+      stat: "40K+",
+      testimonial: "organic views. You really amaze us from the results – we are very amazed by the viewership. By seeing new customers, new faces – it makes us feel motivated and excited.",
+      name: "AJ Delights",
+      title: "Viral Within 15 Days"
     },
     {
-      id: '5n1il9434l',
-      embedCode: `<iframe src="https://fast.wistia.net/embed/iframe/5n1il9434l?videoFoam=true" title="Student Success Story" allow="autoplay; fullscreen" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0"></iframe>`,
-      testimonial: "This is not the first time the course has made me pass a funded challenge, I've passed a few times now",
-      name: "Hamad",
-      title: "Multiple Challenge Passer"
+      id: 6,
+      videoId: "kni3xu5kxu",
+      stat: "20-25%",
+      testimonial: "increase in fresh customers. Before BrandTok, 90% of our customers were regulars. Now we have fresh customers constantly coming in. Most of our customers are from TikTok and I'm very happy.",
+      name: "Jackeline & Wee Seng",
+      title: "Owners of Chice & Chickata"
     }
   ];
 
@@ -56,13 +62,39 @@ const SuccessStories: React.FC = () => {
               <div className="w-layout-grid shotr-form-grid">
                 {testimonials.map((testimonial, index) => (
                   <div key={testimonial.id} className="placeholder _2">
-                    <div 
-                      className="html-embed-4 w-embed w-script"
-                      dangerouslySetInnerHTML={{ __html: testimonial.embedCode }}
-                    />
+                    <div className="html-embed-4 w-embed w-script">
+                      {/* Wistia Video Embed - Using the same approach as the main video */}
+                      <div className="wistia_responsive_padding" style={{padding: '56.25% 0 0 0', position: 'relative'}}>
+                        <div className="wistia_responsive_wrapper" style={{height: '100%', left: 0, position: 'absolute', top: 0, width: '100%'}}>
+                          <div className={`wistia_embed wistia_async_${testimonial.videoId} seo=false videoFoam=true`} style={{height: '100%', position: 'relative', width: '100%'}}>
+                            <div className="wistia_swatch" style={{height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%'}}>
+                              <img 
+                                src={`https://fast.wistia.com/embed/medias/${testimonial.videoId}/swatch`} 
+                                style={{filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%'}} 
+                                alt="" 
+                                aria-hidden="true" 
+                                onLoad={(e) => (e.target as HTMLImageElement).parentElement!.style.opacity = '1'} 
+                              />
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
                     <div className="testimonial-content">
-                      <p className="testimonial-text">{testimonial.testimonial}</p>
+                      <p className="testimonial-text">
+                        <span style={{ 
+                          fontSize: '30px', 
+                          fontWeight: 700, 
+                          display: 'block', 
+                          marginBottom: '10px',
+                          color: '#70b1ff'
+                        }}>
+                          {testimonial.stat}
+                        </span>
+                        {testimonial.testimonial}
+                      </p>
                       <p className="testimonial-name">{testimonial.name}</p>
+                      <p className="testimonial-title">{testimonial.title}</p>
                     </div>
                   </div>
                 ))}
